602d45baf23be2589e9659e01274052819214f7f
MladenTodorovic <44838155+MladenTodorovic@users.noreply.github.com>
2019-04-15 11:28:11 +0200

    Add files via upload

---
+++ globe_bank/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!-- c:\xampp\htdocs\globe_bank\index.php -->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        fdgdfgdf
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
